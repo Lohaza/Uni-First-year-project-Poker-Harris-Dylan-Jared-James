c650d2628c1ed1c5a760c674ed2ae57160ac920b
Lohaza <Harrisriaz0@gmail.com>
2026-04-28 22:04:09 +0100

    testing page, has a bit more random testing doodles

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,5 @@
 import pypokerprob
-print(pypokerprob.draw_cards_random(3))
-pypokerprob.
+
+cards = [[5, 'hearts'], [3, 'hearts'], [1, 'hearts'], [7, 'hearts'], [11, 'hearts'],[13, 'hearts'],[4, 'hearts']]
+print(pypokerprob.evaluate_all_known_cards(cards))
+
